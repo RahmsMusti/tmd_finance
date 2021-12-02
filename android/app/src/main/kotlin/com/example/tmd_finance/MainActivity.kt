f7e51d3d88a1b6aa89a4ee52a7648f62f01944eb
Rahman Mustapha <42654569+RahmsMusti@users.noreply.github.com>
2021-12-02 02:04:52 +0000

    Start of tmd budget from skeleton

---
+++ android/app/src/main/kotlin/com/example/tmd_finance/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.tmd_finance
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/src/sample_feature/sample_item_list_view.dart
@@ -0,0 +1,114 @@
+import 'package:flutter/material.dart';
+
+import '../settings/settings_view.dart';
+import 'sample_item.dart';
+import 'sample_item_details_view.dart';
+
+/// Displays a list of SampleItems.
+class SampleItemListView extends StatelessWidget {
+  SampleItemListView({
+    Key? key,
+    this.items = const [SampleItem(1), SampleItem(2), SampleItem(3)],
+  }) : super(key: key);
+
+  static const routeName = '/';
+
+  final List<SampleItem> items;
+
+  final List<Tab> myTabs = <Tab>[
+    const Tab(text: 'Budget'),
+    const Tab(text: 'Monthly Summary'),
+  ];
+
+  static const Map<String, double> months = {
+    'January': 0.0,
+    'February': 1.2,
+    'March': 1.375,
+    'April': 1.55,
+    'May': 1.725,
+    'June': 3.42,
+  };
+
+  @override
+  Widget build(BuildContext context) {
+    return DefaultTabController(
+      length: myTabs.length,
+      child: Scaffold(
+        appBar: AppBar(
+          title: Column(
+            children: [
+              const SizedBox(
+                height: 10,
+              ),
+              const Text(
+                'tmd: budget',
+              ),
+              DropdownButton<double>(
+                icon: const Icon(Icons.arrow_drop_down_sharp),
+                items: months
+                    .map((name, value) {
+                      return MapEntry(
+                        name,
+                        DropdownMenuItem<double>(
+                          value: value,
+                          child: Text(name),
+                        ),
+                      );
+                    })
+                    .values
+                    .toList(),
+              )
+            ],
+          ),
+          bottom: TabBar(
+            tabs: myTabs,
+          ),
+          actions: [
+            IconButton(
+              icon: const Icon(Icons.settings),
+              onPressed: () {
+                // Navigate to the settings page. If the user leaves and returns
+                // to the app after it has been killed while running in the
+                // background, the navigation stack is restored.
+                Navigator.restorablePushNamed(context, SettingsView.routeName);
+              },
+            ),
+          ],
+        ),
+
+        // To work with lists that may contain a large number of items, it’s best
+        // to use the ListView.builder constructor.
+        //
+        // In contrast to the default ListView constructor, which requires
+        // building all Widgets up front, the ListView.builder constructor lazily
+        // builds Widgets as they’re scrolled into view.
+        body: ListView.builder(
+          // Providing a restorationId allows the ListView to restore the
+          // scroll position when a user leaves and returns to the app after it
+          // has been killed while running in the background.
+          restorationId: 'sampleItemListView',
+          itemCount: items.length,
+          itemBuilder: (BuildContext context, int index) {
+            final item = items[index];
+
+            return ListTile(
+                title: Text('SampleItem ${item.id}'),
+                leading: const CircleAvatar(
+                  // Display the Flutter Logo image asset.
+                  foregroundImage: AssetImage('assets/images/flutter_logo.png'),
+                ),
+                onTap: () {
+                  // Navigate to the details page. If the user leaves and returns to
+                  // the app after it has been killed while running in the
+                  // background, the navigation stack is restored.
+                  Navigator.restorablePushNamed(
+                    context,
+                    SampleItemDetailsView.routeName,
+                  );
+                });
+          },
+        ),
+      ),
+    );
+  }
+}
